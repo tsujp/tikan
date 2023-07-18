6a8eb587a91a53a6d4f243257a955b1aed8de320
Jordan Ellis Coppard <jc+git@wz.ht>
2023-07-19 00:26:46 +1000

    add 2d camera; set clear colour

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,27 +1,33 @@
 use bevy::{prelude::*, window::WindowResolution};
 
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 pub struct TikanPlugin;
 
 impl Plugin for TikanPlugin {
-    fn build(&self, app: &mut App) {}
-}
-
-fn main() {
-    App::new()
-        // Customise window properties as part of DefaultPlugins PluginGroup.
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            // Customise window properties as part of DefaultPlugins PluginGroup.
             primary_window: Some(Window {
                 title: "Tikan - Zero Knowledge Fog of War Chess".to_string(),
-                // position: WindowPosition::Centered(MonitorSelection::Current),
                 resolution: WindowResolution::new(1024., 768.),
                 resizable: false,
                 ..default()
             }),
             ..default()
         }))
-        .run();
+        .insert_resource(ClearColor(Color::Rgba {
+            red: 0.,
+            green: 1.,
+            blue: 0.1,
+            alpha: 1.0,
+        }))
+        .add_systems(Startup, setup);
+    }
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn main() {
+    App::new().add_plugins(TikanPlugin).run();
 }
