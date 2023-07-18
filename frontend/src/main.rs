2d3b269b945884dd35e8562bce01fb15d9ced696
Jordan Ellis Coppard <jc+git@wz.ht>
2023-07-18 23:58:57 +1000

    add minimal bevy window
    - learning Rust and Bevy at the same time, bit by bit.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,27 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+pub struct TikanPlugin;
+
+impl Plugin for TikanPlugin {
+    fn build(&self, app: &mut App) {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        // Customise window properties as part of DefaultPlugins PluginGroup.
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Tikan - Zero Knowledge Fog of War Chess".to_string(),
+                // position: WindowPosition::Centered(MonitorSelection::Current),
+                resolution: WindowResolution::new(1024., 768.),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
 }
