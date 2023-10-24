20b581703e13d75eb52d032036889c4bf8f6dc79
Jordan Ellis Coppard <jc+git@wz.ht>
2023-10-25 02:08:55 +1100

    add argument passthrough to conversions script

--- circuit/lib/test.sh
+++ circuit/lib/test.sh
@@ -257,7 +257,8 @@ format_line ()
 #   the processing we're doing takes at least some time and will mess up the
 #   output order without this.
 printf 'RUN_ID: %s\n' "$datetime"
-nargo test --workspace --show-output 2>&1 |
+# nargo test --workspace --show-output 2>&1 |
+nargo test --show-output "$@" 2>&1 |
   while IFS= read -r ln; do
     format_line
   done
