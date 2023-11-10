2c19a871f509256da465cd360ddef45c2b08cb30
Jordan Ellis Coppard <jc+git@wz.ht>
2023-11-10 15:25:34 +1100

    update bb_help to display 2x6 bitboards

--- circuit/lib/bb_help.ts
+++ circuit/lib/bb_help.ts
@@ -5,22 +5,19 @@ function Big (bi: string) {
    return BigInt(bi.replace(/_/g, ''))
 }
 
-// const x = BigInt('0x00FF00000000FF00')
-// const x = Big('0x00_FF_00_00_00_00_FF_00') // Game start both army pawns.
-// const x = Big('0x08_00_00_00_00_00_00_08')
-// const x = Big('0x10_00_00_00_00_00_00_10')
-// const x = Big('0xff000000001f00')
-// const x = Big('0xf008')
-const x = Big('0x081f000000000000')
-// const x = Big('0x1000000000000010')
+function board (bb: string | string[]) {
+   if (Array.isArray(bb)) {
+      return bb.map((b) => Big(b))
+   }
+
+   return [Big(bb)]
+}
 
 const digits = Math.ceil(64 / Math.log2(2))
 
-// const whitePawns = BigInt('0xFF00') // Same as: pawnPattern << 8n
-// const blackPawns = BigInt('0xFF000000000000') // Same as: pawnPattern << (8n * 6n)
-
 // Configurable.
 const idnt_pre = 0 // Overall output indent.
+const cell_width = 2 // Fixed-width content of board cells.
 const idnt_gap = 2 // Space between board cells.
 const idnt_idx = 3 // Amount of extra space index printing adds (should be computed instead).
 const idnt_tab = 3 // Space between last character of `title_rank` and start of board cells.
@@ -40,32 +37,91 @@ const f_to_rank = ' '.repeat(idnt_to_rank)
 const idnt_from_rank = Math.floor(title_rank.length / 2) - 1 // -1 for rank character's absence.
 const f_from_rank = ' '.repeat(idnt_from_rank)
 
-// Indices are printed in BERLEF ordering as per indexing in Tikan.
-function print_bb (bb: bigint, with_indices: boolean = false) {
+const BOARD_SYMBOLS = ['w', 'b', 'N', 'B', 'R', 'Q', 'K', 'P']
+
+// Indices are printed in LERLEF ordering as per indexing in Tikan.
+function print_bb (
+   bbs: bigint[],
+   with_indices: boolean = false,
+   empty_square_character: string = '0',
+) {
+   if ([1, 8].includes(bbs.length) == false) {
+      throw new Error(
+         'Input bitboards must be an array of either 1 bitboard or 8 bitboards.',
+      )
+   }
+
+   // If it's a single bitboard the piece code to print is '1' (for a set bit).
+   const single_bb = bbs.length === 1
+
+   // Bitboards `S`liced `R`rank `R`eversed.
+   const bbs_srr = bbs.map((bb, i) => {
+      let m = bb
+         .toString(2)
+         .padStart(digits, '0')
+         .replaceAll('0', empty_square_character)
+         .match(/.{8}/g)
+
+      if (m == null) {
+         throw new Error('Could not parse input bitboards into string')
+      }
+
+      let r_rev = m.flatMap((r) => Array.from(r).reverse())
+
+      return r_rev
+   })
+
+   let pop_arr = [...new Array(64)]
+
+   if (single_bb) {
+      pop_arr = [...bbs_srr[0]]
+   } else {
+      const armies_bb = [bbs_srr[0], bbs_srr[1]]
+      const [, , ...pieces_bb] = bbs_srr
+
+      armies_bb.forEach((army_bb, ai) => {
+         pieces_bb.forEach((piece_bb, pi) => {
+            piece_bb.forEach((_, sq_idx) => {
+               if (army_bb[sq_idx] === '1' && piece_bb[sq_idx] === '1') {
+                  // console.log('army', ai, 'has piece at', sq_idx)
+                  pop_arr[sq_idx] = `${BOARD_SYMBOLS[ai]}${BOARD_SYMBOLS[pi + 2]}`
+               } else {
+                  // Only set empty square character if the square hasn't been processed yet.
+                  pop_arr[sq_idx] == null &&
+                     (pop_arr[sq_idx] = empty_square_character)
+               }
+            })
+         })
+      })
+   }
+
    // Compute `title_indices` padding so it's centred over board cells.
    const f_title_indices = ' '.repeat(
-      idnt_tab +
-         Math.floor(
-            (7 * (idnt_gap + (with_indices === true ? idnt_idx : 0)) + 8) / 2,
-         ) - Math.floor(title_indices.length / 2),
+      idnt_tab + Math
+         .floor(
+            (7 *
+                  ((cell_width - 1) + idnt_gap +
+                     (with_indices === true ? idnt_idx : 0)) + 8) / 2,
+         ) - Math
+         .floor(title_indices.length / 2),
    )
 
    // Title line.
    console.log(`${f_pre}${title_rank}${f_title_indices}${title_indices}`)
 
-   // Print ranks and board cells.
-   Array.from(bb.toString(2).padStart(digits, '0')).forEach((c, i) => {
+   pop_arr.forEach((c: string, i) => {
       // Width of visible characters of true-branch should be equal to `idnt_idx`
       //   cbf computing that now, this is already overengineered enough.
       const indices = with_indices === true
-         ? ` \x1b[38;5;243m${i.toString().padStart(2, ' ')}\x1b[0m`
+         ? ` \x1b[38;5;243m${(i ^ 56).toString().padStart(2, ' ')}\x1b[0m`
          : ''
-      const square = `${c}${indices}`
+      const char = `${c}`.padStart(cell_width)
+      const square = `${char}${indices}`
 
       if (i % 8 === 0) {
          // Print rank and first a-file board cell as required.
          process.stdout.write(
-            `\n${f_pre}${f_to_rank}${8 - (i / 8)}${f_from_rank}${f_tab}${square}`,
+            `\n${f_pre}${f_to_rank}${8 - (i >> 3)}${f_from_rank}${f_tab}${square}`,
          )
       } else {
          // Else print each b through h-file board cell.
@@ -77,9 +133,11 @@ function print_bb (bb: bigint, with_indices: boolean = false) {
 
    // +1 for rank characters absence.
    const f_file_lead = ' '.repeat(
-      idnt_pre + idnt_to_rank + idnt_from_rank + idnt_tab + 1,
+      idnt_pre + idnt_to_rank + idnt_from_rank + idnt_tab + 2,
    )
-   const f_file_gap = ' '.repeat(idnt_gap + (with_indices === true ? idnt_idx : 0))
+   const f_file_gap = ' '.repeat(
+      (cell_width - 1) + idnt_gap + (with_indices === true ? idnt_idx : 0),
+   )
 
    // Print files.
    process.stdout.write(f_file_lead)
@@ -89,61 +147,21 @@ function print_bb (bb: bigint, with_indices: boolean = false) {
    console.log(`${title_file}\n\n`)
 }
 
-// print_bb(x)
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 
-print_bb(x, true)
+const x = board('0x100900058000011')
+const b1 = board([
+   '0x100058000010',
+   '0x1010000020001000',
+   '0x100048000010',
+   '0',
+   '0x10000000',
+   '0',
+   '0',
+   '0x1010000020001000',
+])
 
-// console.log(
-//    `black bb: ${blackPawns.toString(2).padStart(digits, '0')} (${blackPawns})`,
-// )
-// console.log(
-//    `white bb: ${whitePawns.toString(2).padStart(digits, '0')} (${whitePawns})`,
-// )
+print_bb(x, false, '.')
 
-// Helpers for serialising bitboard inputs TO the circuit and
-//   deserialising bitboard outs FROM the circuit.
-
-// const BITS_PER_PIECE = 1n
-
-// const maxVal = BigInt('0xFFFFFFFFFFFFFFFF')
-// const pawnPattern = BigInt('0xFF')
-
-// console.log(digits)
-
-// console.log()
-
-// console.log(`add two numbers: ${blackPawns + whitePawns}`)
-
-// // ----- ASCII string to hex (goal FEN -> HEX for circuit input)
-// const minimalFen = '4k3/pppppppp/8/8/8/8/PPPPPPPP/4K3 w - - 0 1'
-
-// // FIXME:
-// // Works but not needed for now just want a hex-byte array to paste into
-// //   lib/lib.nr source code as a literal until Nargo 0.10.3 and can use
-// //   strings instead.
-// // for (let i = 0; i < minimalFen.length; i++) {
-// //    const asciiCode = minimalFen.charCodeAt(i)
-// //    console.log(minimalFen[i], asciiCode, asciiCode.toString(16))
-// // }
-// // -----
-
-// const bytes = []
-
-// for (let i = 0; i < minimalFen.length; i++) {
-//    const asciiCode = minimalFen.charCodeAt(i)
-//    bytes.push(`0x${asciiCode.toString(16)}`)
-// }
-
-// console.log(bytes)
-
-// // console.log(b.toString(2).padStart(digits, '0'))
-
-// // const a = pawns << 8n
-// // const b = maxVal >> 16n
-// // const c = b ^ maxVal
-
-// // console.log(pawns.toString(2).padStart(digits, '0'))
-// // console.log(a.toString(2).padStart(digits, '0'))
-// // console.log(b.toString(2).padStart(digits, '0'))
-// // console.log(c.toString(2).padStart(digits, '0'))
-// // console.log(maxVal.toString(2).padStart(digits, '0'))
+print_bb(b1, false, '.')
