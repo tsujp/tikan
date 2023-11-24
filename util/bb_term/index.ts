d969dcb7b95ca8b6601f60836a34262326c4a60d
Jordan Ellis Coppard <jc+git@wz.ht>
2023-11-24 15:26:15 +1100

    add code formatting script and format code (javascript, typescript, json)

--- util/bb_term/index.ts
+++ util/bb_term/index.ts
@@ -1,16 +1,16 @@
 // Helper so I can declare a BigInt using underscores as seperators for easier
 //   visual grepping.
 function Big (bi: string) {
-   // TODO: Check valid hex with underscores?
-   return BigInt(bi.replace(/_/g, ''))
+    // TODO: Check valid hex with underscores?
+    return BigInt(bi.replace(/_/g, ''))
 }
 
 function board (bb: string | string[]) {
-   if (Array.isArray(bb)) {
-      return bb.map((b) => Big(b))
-   }
+    if (Array.isArray(bb)) {
+        return bb.map((b) => Big(b))
+    }
 
-   return [Big(bb)]
+    return [Big(bb)]
 }
 
 const digits = Math.ceil(64 / Math.log2(2))
@@ -41,110 +41,107 @@ const BOARD_SYMBOLS = ['w', 'b', 'N', 'B', 'R', 'Q', 'K', 'P']
 
 // Indices are printed in LERLEF ordering as per indexing in Tikan.
 function print_bb (
-   bbs: bigint[],
-   with_indices: boolean = false,
-   empty_square_character: string = '0',
+    bbs: bigint[],
+    with_indices: boolean = false,
+    empty_square_character: string = '0',
 ) {
-   if ([1, 8].includes(bbs.length) == false) {
-      throw new Error(
-         'Input bitboards must be an array of either 1 bitboard or 8 bitboards.',
-      )
-   }
+    if ([1, 8].includes(bbs.length) == false) {
+        throw new Error(
+            'Input bitboards must be an array of either 1 bitboard or 8 bitboards.',
+        )
+    }
 
-   // If it's a single bitboard the piece code to print is '1' (for a set bit).
-   const single_bb = bbs.length === 1
+    // If it's a single bitboard the piece code to print is '1' (for a set bit).
+    const single_bb = bbs.length === 1
 
-   // Bitboards `S`liced `R`rank `R`eversed.
-   const bbs_srr = bbs.map((bb, i) => {
-      let m = bb
-         .toString(2)
-         .padStart(digits, '0')
-         .replaceAll('0', empty_square_character)
-         .match(/.{8}/g)
+    // Bitboards `S`liced `R`rank `R`eversed.
+    const bbs_srr = bbs.map((bb, i) => {
+        let m = bb
+            .toString(2)
+            .padStart(digits, '0')
+            .replaceAll('0', empty_square_character)
+            .match(/.{8}/g)
 
-      if (m == null) {
-         throw new Error('Could not parse input bitboards into string')
-      }
+        if (m == null) {
+            throw new Error('Could not parse input bitboards into string')
+        }
 
-      let r_rev = m.flatMap((r) => Array.from(r).reverse())
+        let r_rev = m.flatMap((r) => Array.from(r).reverse())
 
-      return r_rev
-   })
+        return r_rev
+    })
 
-   let pop_arr = [...new Array(64)]
+    let pop_arr = [...new Array(64)]
 
-   if (single_bb) {
-      pop_arr = [...bbs_srr[0]]
-   } else {
-      const armies_bb = [bbs_srr[0], bbs_srr[1]]
-      const [, , ...pieces_bb] = bbs_srr
+    if (single_bb) {
+        pop_arr = [...bbs_srr[0]]
+    } else {
+        const armies_bb = [bbs_srr[0], bbs_srr[1]]
+        const [, , ...pieces_bb] = bbs_srr
 
-      armies_bb.forEach((army_bb, ai) => {
-         pieces_bb.forEach((piece_bb, pi) => {
-            piece_bb.forEach((_, sq_idx) => {
-               if (army_bb[sq_idx] === '1' && piece_bb[sq_idx] === '1') {
-                  // console.log('army', ai, 'has piece at', sq_idx)
-                  pop_arr[sq_idx] = `${BOARD_SYMBOLS[ai]}${BOARD_SYMBOLS[pi + 2]}`
-               } else {
-                  // Only set empty square character if the square hasn't been processed yet.
-                  pop_arr[sq_idx] == null &&
-                     (pop_arr[sq_idx] = empty_square_character)
-               }
+        armies_bb.forEach((army_bb, ai) => {
+            pieces_bb.forEach((piece_bb, pi) => {
+                piece_bb.forEach((_, sq_idx) => {
+                    if (army_bb[sq_idx] === '1' && piece_bb[sq_idx] === '1') {
+                        // console.log('army', ai, 'has piece at', sq_idx)
+                        pop_arr[sq_idx] = `${BOARD_SYMBOLS[ai]}${BOARD_SYMBOLS[pi + 2]}`
+                    } else {
+                        // Only set empty square character if the square hasn't been processed yet.
+                        pop_arr[sq_idx] == null &&
+                            (pop_arr[sq_idx] = empty_square_character)
+                    }
+                })
             })
-         })
-      })
-   }
+        })
+    }
 
-   // Compute `title_indices` padding so it's centred over board cells.
-   const f_title_indices = ' '.repeat(
-      idnt_tab + Math
-         .floor(
-            (7 *
-                  ((cell_width - 1) + idnt_gap +
-                     (with_indices === true ? idnt_idx : 0)) + 8) / 2,
-         ) - Math
-         .floor(title_indices.length / 2),
-   )
+    // Compute `title_indices` padding so it's centred over board cells.
+    const f_title_indices = ' '.repeat(
+        idnt_tab + Math
+            .floor(
+                (7 *
+                        ((cell_width - 1) + idnt_gap +
+                            (with_indices === true ? idnt_idx : 0)) + 8) / 2,
+            ) - Math.floor(title_indices.length / 2),
+    )
 
-   // Title line.
-   console.log(`${f_pre}${title_rank}${f_title_indices}${title_indices}`)
+    // Title line.
+    console.log(`${f_pre}${title_rank}${f_title_indices}${title_indices}`)
 
-   pop_arr.forEach((c: string, i) => {
-      // Width of visible characters of true-branch should be equal to `idnt_idx`
-      //   cbf computing that now, this is already overengineered enough.
-      const indices = with_indices === true
-         ? ` \x1b[38;5;243m${(i ^ 56).toString().padStart(2, ' ')}\x1b[0m`
-         : ''
-      const char = `${c}`.padStart(cell_width)
-      const square = `${char}${indices}`
+    pop_arr.forEach((c: string, i) => {
+        // Width of visible characters of true-branch should be equal to `idnt_idx`
+        //   cbf computing that now, this is already overengineered enough.
+        const indices = with_indices === true
+            ? ` \x1b[38;5;243m${(i ^ 56).toString().padStart(2, ' ')}\x1b[0m`
+            : ''
+        const char = `${c}`.padStart(cell_width)
+        const square = `${char}${indices}`
 
-      if (i % 8 === 0) {
-         // Print rank and first a-file board cell as required.
-         process.stdout.write(
-            `\n${f_pre}${f_to_rank}${8 - (i >> 3)}${f_from_rank}${f_tab}${square}`,
-         )
-      } else {
-         // Else print each b through h-file board cell.
-         process.stdout.write(`${f_gap}${square}`)
-      }
-   })
+        if (i % 8 === 0) {
+            // Print rank and first a-file board cell as required.
+            process.stdout.write(
+                `\n${f_pre}${f_to_rank}${8 - (i >> 3)}${f_from_rank}${f_tab}${square}`,
+            )
+        } else {
+            // Else print each b through h-file board cell.
+            process.stdout.write(`${f_gap}${square}`)
+        }
+    })
 
-   console.log('\n') // Two newlines, but visually just one (first ends rank 1s line).
+    console.log('\n') // Two newlines, but visually just one (first ends rank 1s line).
 
-   // +1 for rank characters absence.
-   const f_file_lead = ' '.repeat(
-      idnt_pre + idnt_to_rank + idnt_from_rank + idnt_tab + 2,
-   )
-   const f_file_gap = ' '.repeat(
-      (cell_width - 1) + idnt_gap + (with_indices === true ? idnt_idx : 0),
-   )
+    // +1 for rank characters absence.
+    const f_file_lead = ' '.repeat(idnt_pre + idnt_to_rank + idnt_from_rank + idnt_tab + 2)
+    const f_file_gap = ' '.repeat(
+        (cell_width - 1) + idnt_gap + (with_indices === true ? idnt_idx : 0),
+    )
 
-   // Print files.
-   process.stdout.write(f_file_lead)
-   title_files.forEach((f) => {
-      process.stdout.write(`${f}${f_file_gap}`)
-   })
-   console.log(`${title_file}\n\n`)
+    // Print files.
+    process.stdout.write(f_file_lead)
+    title_files.forEach((f) => {
+        process.stdout.write(`${f}${f_file_gap}`)
+    })
+    console.log(`${title_file}\n\n`)
 }
 
 // ----------------------------------------------------------------------------
@@ -154,26 +151,26 @@ function print_bb (
 // print_bb(x, false, '.')
 //
 const ROOK__SCENARIO_4 = board([
-   '0x100058000010',
-   '0x1010000020001000',
-   '0x100048000010',
-   '0',
-   '0x10000000',
-   '0',
-   '0',
-   '0x1010000020001000',
+    '0x100058000010',
+    '0x1010000020001000',
+    '0x100048000010',
+    '0',
+    '0x10000000',
+    '0',
+    '0',
+    '0x1010000020001000',
 ])
 // print_bb(ROOK__BLOCK_4, true, '.')
 //
 const ROOK__SCENARIO_3 = board([
-   '0x100030000000',
-   '0x1010000025001000',
-   '0x100020000000',
-   '0',
-   '0x10000000',
-   '0',
-   '0',
-   '0x1010000025001000',
+    '0x100030000000',
+    '0x1010000025001000',
+    '0x100020000000',
+    '0',
+    '0x10000000',
+    '0',
+    '0',
+    '0x1010000025001000',
 ])
 print_bb(ROOK__SCENARIO_3, true, '.')
 //
