96ce5b7578aae7b3d4c375dec45a5348f3f4e077
Jordan Ellis Coppard <jc+git@wz.ht>
2024-01-10 23:50:39 +1100

    add square noir emum generator
    - It's a very, very dumb script.

---
+++ circuit/lib/state/src/gimme_that_enum.ts
@@ -0,0 +1,21 @@
+// Gimme that poor man's enum, kid.
+
+const ASCII_OFFSET = 97
+
+const phat_codegen = []
+
+phat_codegen.push('mod Sq {')
+
+for (let idx = 0; idx < 64; idx++) {
+    // x >> 3 === floor division by 8
+    const rank = (idx >> 3) + 1
+
+    // x & 7 === modulo 8
+    const file = String.fromCharCode((idx & 7) + ASCII_OFFSET)
+
+    phat_codegen.push(`    global ${file}${rank}: u6 = ${idx};`)
+}
+
+phat_codegen.push('}')
+
+console.log(phat_codegen.join('\n'))
