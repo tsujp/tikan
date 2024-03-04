e63dbe775df7864104b46add1849181204898922
Jordan Ellis Coppard <jc+git@wz.ht>
2024-03-04 15:26:46 +0800

    .10

--- circuit/xx_player/foo.ts
+++ circuit/xx_player/foo.ts
@@ -2,57 +2,53 @@ export function spawnTest(wd: string) {
     const proc = Bun.spawn(['nargo', 'test', '--silence-warnings', '--show-output'], {
         cwd: wd,
         stdout: 'pipe',
-        // stderr: 'pipe',
-        // stdin: 'pipe',
-        // stdout: 'inherit',
-        // stderr: 'inherit',
-        // ipc(message, childProc) {
-        //     // console.log('PARENT RECEIVED', message, childProc.pid)
-        //     // log(message)
-        //     // console.log('PARENT RECEIVED', message, childProc.pid)
-        // }a
     })
-
-    // proc.send(msg)
 
     return proc
 }
 
 const wd = process.cwd()
-
 const foo = spawnTest(wd)
-
 const stream = await new Response(foo.stdout).text()
 
 console.log('---------------------------------')
 
-const ENCODING_LENGTH = 6
+const KIND_LENGTH = 6
+const SEPERATOR_LENGTH = 1 // The `:` for clarity if run without this file.
+const ENCODING_LENGTH = KIND_LENGTH + SEPERATOR_LENGTH
 let idx = 0
 let kind = ''
 let pyld = ''
 for await (const chr of stream) {
     if (chr === '\n') {
-        logEncoded(kind, pyld)
+        logEncoded(kind.substring(0, KIND_LENGTH), pyld)
         idx = 0
         kind = ''
         pyld = ''
         continue
     }
-    
+
     if (idx < ENCODING_LENGTH) {
         kind += chr
     } else {
         pyld += chr
     }
-    
+
     // if (idx === ENCODING_LENGTH) {
     //     console.log('encoding is:', enc)
     // }
-    
+
     idx++
 }
 
-type LOG_ENCODINGS = 'T_NAME' | 'T_STEP' | 'T_GAME' | 'T_MOVE' | 'T_COMT'
+type LOG_ENCODINGS =
+    | 'T_NAME'
+    | 'T_STEP'
+    | 'T_GAME'
+    | 'T_MOVE'
+    | 'T_COMT'
+    | 'T___IN' // Input to a function.
+    | 'T__OUT' // Output from a function.
 
 function logName(name: string) {
     console.log(`* ${name}`)
@@ -67,6 +63,14 @@ function logCommit(payload: any) {
     console.log(`    -->    ${payload}`)
 }
 
+function logInput(payload: any) {
+    console.log(`    <-     ${payload}`)
+}
+
+function logOutput(payload: any) {
+    console.log(`     ->    ${payload}`)
+}
+
 function logEncoded(tipe: LOG_ENCODINGS, payload: any) {
     switch (tipe) {
         case 'T_NAME': {
@@ -75,6 +79,14 @@ function logEncoded(tipe: LOG_ENCODINGS, payload: any) {
         }
         case 'T_STEP': {
             logStep(payload)
+            break
+        }
+        case 'T___IN': {
+            logInput(payload)
+            break
+        }
+        case 'T__OUT': {
+            logOutput(payload)
             break
         }
         case 'T_COMT': {
@@ -97,63 +109,9 @@ function logEncoded(tipe: LOG_ENCODINGS, payload: any) {
     }
 }
 
-
-// ---
-// let idx = 0
-// let tipe = ''
-// for await (const chr of stream) {
-//    console.write(chr) 
-
-//     if (chr === '\n') {
-//         idx = 0
-//         tipe = ''
-//         continue
-//         // TODO: Parse it according to tipe
-//     }
-    
-//     if (idx < 4) {
-//         tipe += chr
-//         // console.log('tipe is', tipe, 0 + (2 ** parseInt(chr)), typeof tipe)
-//     }
-    
-//     if (idx === 4) {
-//         // const encoding = parseInt(tipe.reverse().join(''), 2)
-//         const encoding = parseInt(tipe, 2)
-//         console.log('encoding is:', encoding)
-//     }
-
-//     // console.log('next line is', ln)
-//     idx++
-//     console.log()
-    
-//     // console.log(parseInt('0001', 2))
-//     // console.log(parseInt('0010', 2))
-//     // console.log(parseInt('0100', 2))
-//     // console.log(2 ** 0)
-//     // console.log(2 ** 1)
-// }
-// ---
-
 process.on('SIGINT', () => {
     console.log('Cleaning up...')
     // fsEvents.close();
     // server.stop()
     process.exit(0)
 })
-
-
-/*
-Could make it prettier by only grabbing stderr, etc
-
-[devshell] circuit/xx_player $ nargo test --show-output --silence-warnings 2> /dev/null
-[xx_player] Running 1 test function
-
-T_NAMEwhite compass-rose moves 1 to 0
-T_NAMEwhite compass-rose moves 1 to 5
-T_NAMEwhite compass-rose moves 1 to 6
-T_NAMEwhite compass-rose moves 1 to 7
-T_NAMEwhite compass-rose moves 1 to 2
-[devshell] circuit/xx_player $ nargo test --show-output --silence-warnings 1> /dev/null
-[xx_player] Testing compass_moves__white__1... ok
-[xx_player] 1 test passed
-*/
--- flake.nix
+++ flake.nix
@@ -1,21 +1,19 @@
 {
-    description = "Tikan -- Fog of War Chess ";
-
-    # TODO: Can Nix also install the bun packages too? Or would that just be a hook to run `bun i` lol.
+    description = "Tikan -- Fog of War Chess";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
         flake-parts.url = "github:hercules-ci/flake-parts";
         devshell.url = "github:numtide/devshell";
-        noir.url = "github:noir-lang/noir/5be9f9d7e2f39ca228df10e5a530474af0331704";
+        noir.url = "github:noir-lang/noir/cb4c1c5264b95d01f69d99f916ced71ad9cdc9d1";
+        # noir.url = "github:tsujp/noir/0cf043d81e2da7c4b23c4b18e0c3a944dd7ea017";
+        # noir.url = "github:tsujp/noir/5be9f9d7e2f39ca228df10e5a530474af0331704";
     };
 
-    outputs = inputs@{ self, nixpkgs, devshell, flake-parts, noir }:
+    outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
         flake-parts.lib.mkFlake { inherit inputs; } {
             imports = [
-                devshell.flakeModule
-                # noir.packages
-                # flake-parts.flakeModules.easyOverlay
+                inputs.devshell.flakeModule
             ];
 
             systems = [
@@ -26,54 +24,12 @@
             ];
 
             perSystem = { pkgs, ... }: rec {
-              # Docs: https://flake.parts/overlays#an-overlay-for-free-with-flake-parts
-                # overlayAttrs = {
-                #     inherit (noir.packages) nargo;
-                # };
+                packages.noir = inputs.noir.packages;
 
-                # packages.default = pkgs.buildGo121Module {
-                #     name = "bingbongbaz";
-
-                #     src = ./.;
-
-                #     vendorHash = "sha256-Wn1q+IPjf9EnCXpgzUHmsg2BbGS9mE0Lbk4kQ145n2s=";
-
-                #     # Build flags.
-                #     CGO_ENABLED = 0;
-                #     flags = [ "-trimpath" ];
-                #     ldflags = [
-                #         "-s"
-                #         "-w"
-                #         "-extldflags -static"
-                #     ];
-                # };
-
-                packages.nargo = noir.packages.nargo;
-
-                # `nix develop`.
                 devshells.default = {
-                    # Packages present in the default shell.
                     packages = with pkgs; [
-                      bun
-                    ] ++ [ noir.packages.${system}.nargo ];
-
-                    # Commands present in the default shell.
-                    # commands = [
-                        # {
-                        #     # TODO: Probably `go install` instead?
-                        #     category = "[codebase]";
-                        #     name = "build";
-                        #     help = "build project for production";
-                        #     command = "nix build";
-                        # }
-                        # {
-                        #     category = "[codebase]";
-                        #     name = "dev";
-                        #     help = "run project for development";
-                        #     # TODO: Use same env vars from `nix build`?
-                        #     command = "CGO_ENABLED=0 go run main.go $@";
-                        # }
-                    # ];
+                        bun
+                    ] ++ [ packages.noir.${system}.nargo ];
                 };
             };
         };
